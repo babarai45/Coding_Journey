63c4aefc72b1271e35fc01001077eb500c9e6c13
babarai45 <muhammadbabarai45@gmail.com>
2025-05-28 22:05:52 +0500

    :tada: day01

---
+++ Day01/C/assignment01.c
@@ -0,0 +1,78 @@
+// Q1 write a progaram to print Hello Student on the screen
+// #include <stdio.h>
+// int main()
+// {
+//     printf("Hello Student\n");
+//     return 0;
+// }
+
+// // Q2 write a program to print Hello on first line and Student on second line
+// #include <stdio.h>
+// int main()
+// {
+//     printf("Hello \nStudent");
+//     // if we  add space after formater specifier it will show a
+//     return 0;
+// }
+
+//
+// // Q3  write a program to print "Muhammad Babar" as same in double quotes
+// #include <stdio.h>
+// int main()
+// {
+//     printf("\"Muhammad Babar\"\n");
+//     printf("\'Muhammad Babar\'");
+//     return 0;
+// }\
+
+
+// // // Q4  write a program to print "\n" on screen
+// #include <stdio.h>
+// int main()
+// {
+//     printf("\\n\n");
+//     printf("\\r");
+//     return 0;
+// }
+
+
+// // Q5  write a program to print sum of two integers make sure take input from user
+// #include <stdio.h>
+// int main()
+// {
+//     int a,b;
+//     printf("Enter two numbers: ");
+//     scanf("%d %d",&a,&b);
+//     printf("The sum is a and b is : %d",a+b);
+//
+//     return 0;
+// }
+
+
+// // Q6  write a program to calculate square of user entered number
+//
+// #include <stdio.h>
+// int main()
+// {
+//     int a;
+//     printf("Enter a number: ");
+//     scanf("%d", &a);
+//     printf("The Entered Number Square is : %d", a*a);
+//     return 0;
+// }
+
+
+// // Q3  write a program to print "Muhammad Babar" as same in double quotes
+//
+#include <stdio.h>
+int main()
+{
+    int l,w;
+    printf("Enter the length and width of rectangle: ");
+    scanf("%d %d",&l,&w);
+    printf("Area of rectangle: %d\n",l*w);
+
+    return 0;
+}
+
+// // Q7  write a program to print "Muhammad Babar" as same in double quotes
---
+++ Day01/C/day01.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+printf("Hello World deep dive! \n");
+
+return 0;
+
+}
